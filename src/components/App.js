b8b1e32e757373c8d5c542332c4fd6035806131f
rajukumardas <100093547+rajukumardas@users.noreply.github.com>
2022-11-18 15:36:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+         <h1 data-ns-test="project-name">Project Name</h1>
+      <h2 data-ns-test="project-description">Project Description</h2>
+    </div>
   )
 }
 
